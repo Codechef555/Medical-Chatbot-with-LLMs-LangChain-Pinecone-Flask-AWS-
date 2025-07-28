6c141c52f83bc59cc19f6a02f13571f63f5418db
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2025-07-28 08:04:59 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Md.Karaamathullah sheriff",
+    author_email="mdkaraamathullahsheriff@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
